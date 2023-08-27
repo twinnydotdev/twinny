ad290ef8573561f9877989066b2c8ee7cff60d47
rjmacarthy <richardmacarthy@hotmail.com>
2023-08-27 19:35:53 +0100

    apply options to api call

--- src/types.ts
+++ src/types.ts
@@ -2,4 +2,6 @@ import { CreateCompletionRequest } from 'openai'
 
 export type CompletionRequest = CreateCompletionRequest & {
   one_line: boolean | undefined
+  top_k: number | undefined
+  num_return_sequences: number | undefined
 }
