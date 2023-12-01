4a188fc5ea12db4fb32c469e2cab9d6222d876ec
rjmacarthy <richardmacarthy@hotmail.com>
2023-12-01 14:50:24 +0000

    remove unused file

--- src/types.ts
+++
@@ -1,9 +0,0 @@
-import { CreateCompletionRequest } from 'openai'
-
-export type CompletionRequest = CreateCompletionRequest & {
-  max_time: number | undefined
-  one_line: boolean | undefined
-  top_k: number | undefined
-  repetition_penalty: number | undefined
-  num_return_sequences: number | undefined
-}
