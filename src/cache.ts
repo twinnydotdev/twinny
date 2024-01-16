8ac64678993fe45ef9b984e08f4bf395dc7539fe
rjmacarthy <richardmacarthy@hotmail.com>
2024-01-16 11:04:49 +0000

    update cache lru

--- src/cache.ts
+++ src/cache.ts
@@ -1,4 +1,33 @@
-const cache: { [key: string]: string | null } = {}
+class LRUCache {
+  private capacity: number
+  private cache: Map<string, string | null>
+
+  constructor(capacity: number) {
+    this.capacity = capacity
+    this.cache = new Map()
+  }
+
+  get(key: string): string | null | undefined {
+    if (!this.cache.has(key)) return undefined
+
+    const value = this.cache.get(key)
+    this.cache.delete(key)
+    this.cache.set(key, value || '')
+    return value
+  }
+
+  set(key: string, value: string | null): void {
+    if (this.cache.has(key)) {
+      this.cache.delete(key)
+    } else if (this.cache.size === this.capacity) {
+      const firstKey = this.cache.keys().next().value
+      this.cache.delete(firstKey)
+    }
+    this.cache.set(key, value)
+  }
+}
+
+const cache = new LRUCache(50)
 
 function normalize(src: string) {
   return src.split('\n').join('').replace(/\s+/gm, '').replace(' ', '')
@@ -16,7 +45,7 @@ export function getCache(args: {
   suffix: string | null
 }): string | undefined | null {
   const key = getKey(args)
-  return cache[key]
+  return cache.get(key)
 }
 
 export function setCache(args: {
@@ -25,5 +54,5 @@ export function setCache(args: {
   completion: string | null
 }) {
   const key = getKey(args)
-  cache[key] = args.completion
+  cache.set(key, args.completion)
 }
