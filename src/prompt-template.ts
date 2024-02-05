1c7b06441bf3beb511442c29d98c90db3df481f9
rjmacarthy <richardmacarthy@hotmail.com>
2024-02-05 10:43:24 +0000

    small fix

--- src/prompt-template.ts
+++ src/prompt-template.ts
@@ -10,7 +10,7 @@ export const getFimPromptTemplateLLama = ({
   const fileContext = useFileContext ? context : ''
   const heading = header ? header : ''
   return {
-    prompt: `<PRE> ${fileContext}\n${heading}${prefix} <SUF>${suffix} <MID>`,
+    prompt: `<PRE> ${fileContext}\n${heading}${prefix} <SUF> ${suffix} <MID>`,
     prefix,
     suffix,
     stop: ['<EOT>']
