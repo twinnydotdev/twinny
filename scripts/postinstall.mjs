46e80202cee8b6d2950c5d8ecb1ea1303958fdc4
rjmacarthy <richardmacarthy@hotmail.com>
2024-03-19 13:32:09 +0000

    mkdir wasms dir

--- scripts/postinstall.mjs
+++ scripts/postinstall.mjs
@@ -6,10 +6,13 @@ import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const targetDir = path.join(__dirname, '../out/tree-sitter-wasms')
+fs.mkdirSync(targetDir, { recursive: true })
+
 await new Promise((resolve, reject) => {
   ncp(
     path.join(__dirname, '../node_modules/tree-sitter-wasms/out'),
-    path.join(__dirname, '../out/tree-sitter-wasms'),
+    targetDir,
     (error) => {
       if (error) {
         reject(error)
@@ -19,6 +22,9 @@ await new Promise((resolve, reject) => {
     }
   )
 })
+
+const wasmTargetDir = path.join(__dirname, '../out')
+fs.mkdirSync(wasmTargetDir, { recursive: true })
 
 fs.copyFileSync(
   path.join(__dirname, '../node_modules/web-tree-sitter/tree-sitter.wasm'),
