ee304cc17e38ba20d5e12355def623be4519e582
rjmacarthy <richardmacarthy@hotmail.com>
2024-03-25 19:56:44 +0000

    inline completion fixes

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -211,6 +211,7 @@ export const MULTI_LINE_NODE_TYPE = [
   'object_type',
   'interface_body',
   'class_body',
+  'export_statement',
   // react
   'jsx_opening_element',
   'jsx_self_closing_element',
