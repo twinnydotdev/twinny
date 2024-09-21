478f858bab705081523efae435a3dd114305fd2b
rjmacarthy <richardmacarthy@hotmail.com>
2024-09-21 13:38:00 +0100

    fix template prompts

--- src/extension/chat-service.ts
+++ src/extension/chat-service.ts
@@ -702,7 +702,7 @@ export class ChatService {
     })
 
     if (!provider.modelName.includes('claude')) {
-      conversation.push(systemMessage)
+      conversation.unshift(systemMessage)
     }
 
     return conversation
