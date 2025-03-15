a156054314c6a565a898f40107601ec456828e15
rjmacarthy <richardmacarthy@hotmail.com>
2025-03-15 12:19:49 +0000

    faster model loading symmetry

--- src/extension/symmetry-ws.ts
+++ src/extension/symmetry-ws.ts
@@ -1,59 +1,78 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import * as vscode from "vscode"
-import { WebSocket } from "ws"
+import * as vscode from "vscode";
+import { WebSocket } from "ws";
 
-import { EVENT_NAME, URL_SYMMETRY_WS } from "../common/constants"
-import { ServerMessage } from "../common/types"
+import { EVENT_NAME, URL_SYMMETRY_WS } from "../common/constants";
+import { ServerMessage } from "../common/types";
 
 export class SymmetryWs {
-  private _ws: WebSocket | null = null
-  private _webView: vscode.Webview | undefined
-  private _modelData: any = []
+  private _ws: WebSocket | null = null;
+  private _webView: vscode.Webview | undefined;
+  private _modelData: any = [];
+  private _updateInterval: NodeJS.Timeout | null = null;
 
   constructor(view: vscode.Webview | undefined) {
-    this._webView = view
-    this.registerHandlers()
+    this._webView = view;
+    this.registerHandlers();
+    this.connectSymmetryWs();
   }
 
   public connectSymmetryWs = () => {
-    this._ws = new WebSocket(URL_SYMMETRY_WS)
+    this._ws = new WebSocket(URL_SYMMETRY_WS, {
+      perMessageDeflate: false,
+    });
 
     this._ws.on("message", (data: any) => {
-      try {
-        const parsedData = JSON.parse(data.toString())
-        this._modelData = parsedData?.allPeers?.filter(
-          (peer: any) => peer.healthy
-        )
-        this._webView?.postMessage({
-          type: EVENT_NAME.twinnySymmetryModels,
-          data: this._modelData
-        })
-      } catch (error) {
-        console.error("Error parsing WebSocket message:", error)
-      }
-    })
+      const parsedData = JSON.parse(data.toString());
+      this._modelData = parsedData?.allPeers?.filter((peer: any) => peer.healthy) || [];
+      this.sendUpdateToWebview();
+    });
 
     this._ws.on("error", (error: any) => {
-      console.error("WebSocket error:", error)
-    })
-  }
+      console.error("WebSocket error:", error);
+    });
+
+    this._ws.on("close", () => {
+      console.log("WebSocket closed, attempting reconnect...");
+      this.reconnect();
+    });
+
+    this._updateInterval = setInterval(() => {
+      if (this._ws?.readyState === WebSocket.OPEN) {
+        this._ws.ping();
+      }
+    }, 30000);
+  };
+
+  private sendUpdateToWebview = () => {
+    this._webView?.postMessage({
+      type: EVENT_NAME.twinnySymmetryModels,
+      data: this._modelData,
+    });
+  };
+
+  private reconnect = () => {
+    if (this._ws) {
+      this._ws.removeAllListeners();
+      this._ws.close();
+    }
+    setTimeout(() => this.connectSymmetryWs(), 1000);
+  };
 
   public registerHandlers() {
-    this._webView?.onDidReceiveMessage(
-      async (message: ServerMessage<string>) => {
-        if (message.type === EVENT_NAME.twinnyGetSymmetryModels) {
-          this._webView?.postMessage({
-            type: EVENT_NAME.twinnySymmetryModels,
-            data: this._modelData
-          })
-        }
+    this._webView?.onDidReceiveMessage((message: ServerMessage<string>) => {
+      if (message.type === EVENT_NAME.twinnyGetSymmetryModels) {
+        this.sendUpdateToWebview();
       }
-    )
+    });
   }
 
   public dispose() {
+    if (this._updateInterval) {
+      clearInterval(this._updateInterval);
+    }
     if (this._ws) {
-      this._ws.close()
+      this._ws.close();
     }
   }
 }
