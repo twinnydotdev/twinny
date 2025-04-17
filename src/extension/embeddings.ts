d26bdd30d07383388d1b4a7ae7fd92d82b6bf3d8
Anton <ton-An@outlook.de>
2025-04-17 10:10:27 +0200

    fix embedding network overload + wrong root path when getting file paths (#462)
    * add p-queue as dependency
    
    * fix embedding network overload + wrong root path when getting file paths

--- src/extension/embeddings.ts
+++ src/extension/embeddings.ts
@@ -16,6 +16,7 @@ import {
 
 import { Base } from "./base"
 import { fetchEmbedding } from "./llm"
+import PQueue from "p-queue"
 import { TwinnyProvider } from "./provider-manager"
 import { getDocumentSplitChunks, readGitSubmodulesFile } from "./utils"
 
@@ -27,6 +28,7 @@ export class EmbeddingDatabase extends Base {
   private _workspaceName = vscode.workspace.name || ""
   private _documentTableName = `${this._workspaceName}-documents`
   private _filePathTableName = `${this._workspaceName}-file-paths`
+  private _embeddingQueue = new PQueue({ concurrency: 5 })
 
   constructor(dbPath: string, context: vscode.ExtensionContext) {
     super(context)
@@ -42,46 +44,49 @@ export class EmbeddingDatabase extends Base {
   }
 
   public async fetchModelEmbedding(content: string) {
-    const provider = this.getEmbeddingProvider()
+    return this._embeddingQueue.add(async () => {
+      const provider = this.getEmbeddingProvider()
 
-    if (!provider) return
+      if (!provider) return
 
-    const requestBody: RequestOptionsOllama = {
-      model: provider.modelName,
-      input: content,
-      stream: false,
-      options: {}
-    }
-
-    const requestOptions: RequestOptions = {
-      hostname: provider.apiHostname || "localhost",
-      port: provider.apiPort,
-      path: provider.apiPath || "/api/embed",
-      protocol: provider.apiProtocol || "http",
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${provider.apiKey}`
+      const requestBody: RequestOptionsOllama = {
+        model: provider.modelName,
+        input: content,
+        stream: false,
+        options: {}
       }
-    }
 
-    return new Promise<number[]>((resolve) => {
-      fetchEmbedding({
-        body: requestBody,
-        options: requestOptions,
-        onData: (response) => {
-          resolve(this.getEmbeddingFromResponse(provider, response))
+      const requestOptions: RequestOptions = {
+        hostname: provider.apiHostname || "localhost",
+        port: provider.apiPort,
+        path: provider.apiPath || "/api/embed",
+        protocol: provider.apiProtocol || "http",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${provider.apiKey}`
         }
+      }
+
+      return new Promise<number[]>((resolve) => {
+        fetchEmbedding({
+          body: requestBody,
+          options: requestOptions,
+          onData: (response) => {
+            resolve(this.getEmbeddingFromResponse(provider, response))
+          }
+        })
       })
     })
   }
 
-  private getAllFilePaths = async (dirPath: string): Promise<string[]> => {
+  private getAllFilePaths = async (
+    rootPath: string,
+    dirPath: string
+  ): Promise<string[]> => {
     let filePaths: string[] = []
     const dirents = await fs.promises.readdir(dirPath, { withFileTypes: true })
     const submodules = readGitSubmodulesFile()
-
-    const rootPath = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath || ""
 
     const ig = ignore()
 
@@ -112,7 +117,9 @@ export class EmbeddingDatabase extends Base {
       }
 
       if (dirent.isDirectory()) {
-        filePaths = filePaths.concat(await this.getAllFilePaths(fullPath))
+        filePaths = filePaths.concat(
+          await this.getAllFilePaths(rootPath, fullPath)
+        )
       } else if (dirent.isFile()) {
         filePaths.push(fullPath)
       }
@@ -123,7 +130,7 @@ export class EmbeddingDatabase extends Base {
   public async injestDocuments(
     directoryPath: string
   ): Promise<EmbeddingDatabase> {
-    const filePaths = await this.getAllFilePaths(directoryPath)
+    const filePaths = await this.getAllFilePaths(directoryPath, directoryPath)
     const totalFiles = filePaths.length
     let processedFiles = 0
 
@@ -185,23 +192,15 @@ export class EmbeddingDatabase extends Base {
     try {
       const tableNames = await this._db?.tableNames()
       if (!tableNames?.includes(`${this._workspaceName}-documents`)) {
-        await this._db?.createTable(
-          this._documentTableName,
-          this._documents,
-          {
-            mode: "overwrite"
-          }
-        )
+        await this._db?.createTable(this._documentTableName, this._documents, {
+          mode: "overwrite"
+        })
       }
 
       if (!tableNames?.includes(`${this._workspaceName}-file-paths`)) {
-        await this._db?.createTable(
-          this._filePathTableName,
-          this._filePaths,
-          {
-            mode: "overwrite"
-          }
-        )
+        await this._db?.createTable(this._filePathTableName, this._filePaths, {
+          mode: "overwrite"
+        })
         return
       }
 
