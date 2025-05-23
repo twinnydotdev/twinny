44b838334739917b2355ac20e9b3610eddf583b1
rjmacarthy <richardmacarthy@hotmail.com>
2025-05-23 18:15:16 +0100

    add more languages

--- src/webview/i18n.ts
+++ src/webview/i18n.ts
@@ -5,7 +5,13 @@ import de from "./assets/locales/de.json"
 import en from "./assets/locales/en.json"
 import es from "./assets/locales/es.json"
 import esCL from "./assets/locales/es-CL.json"
+import fr from "./assets/locales/fr.json"
+import it from "./assets/locales/it.json"
 import ja from "./assets/locales/ja.json"
+import ko from "./assets/locales/ko.json"
+import nl from "./assets/locales/nl.json"
+import pt from "./assets/locales/pt.json"
+import ru from "./assets/locales/ru.json"
 import zhCN from "./assets/locales/zh-CN.json"
 import zhHK from "./assets/locales/zh-HK.json"
 
@@ -16,13 +22,19 @@ i18n.use(initReactI18next).init({
     en: { translation: en },
     es: { translation: es },
     esCL: { translation: esCL },
+    fr: { translation: fr },
+    it: { translation: it },
     ja: { translation: ja },
+    ko: { translation: ko },
+    nl: { translation: nl },
+    pt: { translation: pt },
+    ru: { translation: ru },
     "zh-CN": { translation: zhCN },
     "zh-HK": { translation: zhHK }
   },
   detection: {
     order: ["localStorage"],
-    availableLanguages: ["en", "zh-CN", "zh-HK", "ja", "es", "esCL", "de"]
+    availableLanguages: ["en", "zh-CN", "zh-HK", "ja", "es", "esCL", "de", "fr", "it", "pt", "ru", "ko", "nl"]
   }
 })
 
